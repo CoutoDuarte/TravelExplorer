dd3979593de8fbdd900669aaf500dd4bf77a9da8
CoutoDuarte <duartecouto05@gmail.com>
2026-04-02 18:28:56 +0100

    Classes para DAO

---
+++ TravelExplorer/src/main/java/Connection/DBConnection.java
@@ -0,0 +1,16 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	
+	private static final String url = "jdbc:mysql://localhost:3306/univ2026";
+	private static final String username = "root";
+	private static final String password = "admin";
+
+	public static Connection getConnection() throws Exception
+	{
+		return DriverManager.getConnection(url,username,password);
+	}
+}
---
+++ TravelExplorer/src/main/java/Connection/Viagens.java
@@ -0,0 +1,43 @@
+package Connection;
+
+import java.sql.Timestamp;
+
+public class Viagens {
+    private int idViagem;
+    private int numBilhetesAdulto;
+    private int numBilhetesCrianca;
+    private float preco;
+    private String origem;
+    private String destino;
+    private Timestamp dataHoraPartida;
+    private Timestamp dataHoraRegresso;
+    private String descricao;
+    private String empresa;
+
+    public Viagens(int idViagem, int numBilhetesAdulto, int numBilhetesCrianca, float preco,
+                   String origem, String destino, Timestamp dataHoraPartida,
+                   Timestamp dataHoraRegresso, String descricao, String empresa) {
+
+        this.idViagem = idViagem;
+        this.numBilhetesAdulto = numBilhetesAdulto;
+        this.numBilhetesCrianca = numBilhetesCrianca;
+        this.preco = preco;
+        this.origem = origem;
+        this.destino = destino;
+        this.dataHoraPartida = dataHoraPartida;
+        this.dataHoraRegresso = dataHoraRegresso;
+        this.descricao = descricao;
+        this.empresa = empresa;
+    }
+
+    public int getIdViagem() { return idViagem; }
+    public int getNumBilhetesAdulto() { return numBilhetesAdulto; }
+    public int getNumBilhetesCrianca() { return numBilhetesCrianca; }
+    public float getPreco() { return preco; }
+    public String getOrigem() { return origem; }
+    public String getDestino() { return destino; }
+    public Timestamp getDataHoraPartida() { return dataHoraPartida; }
+    public Timestamp getDataHoraRegresso() { return dataHoraRegresso; }
+    public String getDescricao() { return descricao; }
+    public String getEmpresa() { return empresa; }
+}

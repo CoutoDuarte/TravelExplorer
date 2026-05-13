aa3a1c064bbed25b43556767510432beea0c00ea
Filipe Aroso <filipearoso@Filipes-MacBook-Pro.local>
2026-05-13 14:38:10 +0100

    arraneji a db connection pq mandei mal sorry duarte

--- src/main/java/Connection/DBConnection.java
+++ src/main/java/Connection/DBConnection.java
@@ -2,14 +2,26 @@ package Connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/TravelExplorer?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String username = "travel_user";
-    private static final String password = "Root123!";
+    private static final String url = System.getenv().getOrDefault(
+        "TRAVEL_DB_URL",
+        "jdbc:mysql://127.0.0.1:3306/TravelExplorer?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC"
+    );
 
-    public static Connection getConnection() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private static final String username = System.getenv().getOrDefault("TRAVEL_DB_USER", "root");
+    private static final String password = System.getenv().getOrDefault("TRAVEL_DB_PASSWORD", "");
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
     }
 }
